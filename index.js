0fb3c2487f9eaadc00c31f05931c88e852421ac9
Dx724 <dx72dx@gmail.com>
2020-02-02 00:19:27 -0500

    Updated server port binding

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ var server = http.createServer(function(request, response) {
 			response.end();
 		}
 	}
-).listen($PORT);
+).listen(process.env.PORT || 8888);
 console.log("Server started.");
 
 var games = require("./games");
